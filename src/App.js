fd2d1b72013631bc32d2ed20da9f047bec6033af
jonnatay <129225623+jonnatay@users.noreply.github.com>
2023-05-04 23:56:52 -0400

    Attempt3.0.5

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import List from './list'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import EditTask from './editTask';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -49,5 +50,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+export const auth = getAuth(app);
 export default App;
